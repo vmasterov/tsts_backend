931c635195f22d99a0f8f2a67eae8e8c363978ef
Vladimir Masterov <xiii555iiix@yndex.ru>
2021-04-04 11:54:51 +0300

    Add https

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const logger = require('morgan')
 const mongoose = require('mongoose')
 const config = require('./config')
 const passport = require('passport')
+const createError = require('http-errors')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
@@ -22,6 +23,11 @@ connect.then(
 
 const app = express()
 
+app.all('*', (req, res, next) => {
+  if (req.secure) return next()
+  else res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url)
+})
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -30,5 +36,21 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+// error handler
+app.use((err, req, res) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
 
 module.exports = app
--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,11 @@ const User = require('../models/user')
 const router = express.Router()
 const passport = require('passport')
 const authenticate = require('../authenticate')
-// const cors = require('./cors')
+const cors = require('./cors')
 
 router.use(bodyParser.json())
 
-router.get('/', /* cors.corsWithOptions, */ authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
+router.get('/', cors.corsWithOptions, authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   User.find({})
     .then(
       users => {
@@ -21,7 +21,7 @@ router.get('/', /* cors.corsWithOptions, */ authenticate.verifyUser, authenticat
     .catch(error => next(error))
 })
 
-router.post('/singup', /* cors.corsWithOptions, */ function (req, res) {
+router.post('/singup', cors.corsWithOptions, function (req, res) {
   User.register(new User({ username: req.body.username }), req.body.password, (error, user) => {
     if (error) {
       res.statusCode = 500
@@ -51,7 +51,7 @@ router.post('/singup', /* cors.corsWithOptions, */ function (req, res) {
   })
 })
 
-router.post('/singin', /* cors.corsWithOptions, */ passport.authenticate('local'), function (req, res) {
+router.post('/singin', cors.corsWithOptions, passport.authenticate('local'), function (req, res) {
   let token = authenticate.getToken({ _id: req.user._id })
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
@@ -62,7 +62,7 @@ router.post('/singin', /* cors.corsWithOptions, */ passport.authenticate('local'
   })
 })
 
-router.get('/logout', /* cors.corsWithOptions, */ function (req, res, next) {
+router.get('/logout', cors.corsWithOptions, function (req, res, next) {
   // Check if DON`T Facebook use sessions -- remove it
   if (req.session) {
     req.session.destroy()
